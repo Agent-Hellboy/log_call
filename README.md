75746f443efde75cbe17f62690068cfc02d7027b
Agent-Hellboy <princekrroshan01@gmail.com>
2022-09-04 13:52:54 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## log_call 
+
+library to log function or bond-method calls
+
+
+### Example
+```py
+@log_call
+class A:
+    def __init__(self):
+        pass 
+
+    def a(self,*args,**kwargs):
+        pass
+
+
+a=A()
+a.a(45,'str',s=34,g=43)
+
+@log_call
+def c(a,*args,**kwargs):
+    pass
+
+c(34,56,p=23)
+```
+response 
+2022-09-04 12:56:08,552 a called with {'args': [45, 'str'], 'kwargs': {'s': 34, 'g': 43}}
+2022-09-04 12:56:08,552 c called with {'a': 34, 'args': [56], 'kwargs': {'p': 23}}
+
---
+++ src/__init__.py
@@ -0,0 +1,50 @@
+import functools
+import inspect
+import logging
+from typing import Callable, Dict, List, Any
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+
+def prepare_response(func: str, args_dict: Dict) -> str:
+    return f"{func} called with {args_dict}"
+
+
+def get_log_stmt(func: Callable, args_v: List, kwargs_v: Dict) -> str:
+    args_dict: Dict = {}
+    all_args_val = inspect.getfullargspec(func)
+    args = all_args_val.args
+    args_v = list(args_v)
+    if args:
+        for i in range(len(args)):
+            if args[i] != 'self':
+                args_dict[args[i]] = args_v[i]
+                args_v.pop(0)
+    args_dict['args'] = args_v
+    args_dict['kwargs'] = kwargs_v
+    return prepare_response(func.__name__, args_dict)
+
+
+def logger(func: Callable):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        log_stmt = get_log_stmt(func, args, kwargs)
+        logging.info(f"{log_stmt}")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def log_call(obj: Callable[..., Any]) -> Callable:
+    print(type(obj))
+    if type(obj).__name__ == 'function':
+        return logger(obj)
+
+    class Wrapper(obj):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+        def __getattribute__(self, attr):
+            attr = object.__getattribute__(self, attr)
+            return logger(attr) if callable(attr) else attr
+
+    return Wrapper
